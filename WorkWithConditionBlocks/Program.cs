48ee3f306219117692569fe4d93b747cd493e7ae
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2022-01-23 16:54:34 +0300

    Conditionals

---
+++ WorkWithConditionBlocks/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithConditionBlocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var number = 11;
+
+            if (number==10)
+            {
+                Console.WriteLine("number is 10");
+            }
+            else
+            {
+                Console.WriteLine("number is not 10");
+
+            }
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
