0da15d92b07f126ab5817f6e13ea51cb5d580fa6
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2022-01-23 22:05:31 +0300

    for loop

---
+++ Loops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <=100; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Finished");
+
+            Console.ReadLine();
+        }
+    }
+}
