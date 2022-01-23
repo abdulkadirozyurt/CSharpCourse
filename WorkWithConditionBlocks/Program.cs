edbb53115298df72694af6a8094eb8f12b939e85
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2022-01-23 17:31:56 +0300

    multiconditions

--- WorkWithConditionBlocks/Program.cs
+++ WorkWithConditionBlocks/Program.cs
@@ -13,11 +13,11 @@ namespace WorkWithConditionBlocks
 
             var number = 11;
 
-            if (number==10)
+            if (number == 10)
             {
                 Console.WriteLine("number is 10");
             }
-            else
+            else if (number > 100 && number <= 200)
             {
                 Console.WriteLine("number is not 10");
 
