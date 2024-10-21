ffb36aede4367371d8e064360c8b5540bb318937
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2024-10-21 21:26:33 +0300

    inheritance

---
+++ 11.Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _11.Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[]
+            {
+                new Customer{FirstName = "Abdulkadir"},
+                new Student{Department = "Computer Science"}
+
+            };
+
+            foreach (var item in people)
+            {
+                Console.WriteLine(item.FirstName);
+            }
+
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer : Person
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+}
